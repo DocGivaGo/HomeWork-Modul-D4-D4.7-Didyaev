aa0378726bbbe43dff19b79b220370865a2c47e0
DocGivaGo <117738746+DocGivaGo@users.noreply.github.com>
2023-02-10 13:38:24 +0300

    Add files via upload

---
+++ NewsPortalProject/News/views.py
@@ -0,0 +1,93 @@
+from datetime import datetime
+from django.views.generic import ListView, DetailView, CreateView, UpdateView, DeleteView
+from .models import Post
+from .filters import NewsFilter
+from .forms import PostForm
+from django.urls import reverse_lazy
+
+
+class PostsList(ListView):
+    model = Post
+    ordering = '-dateCreation'
+    template_name = 'news.html'
+    context_object_name = 'news'
+    paginate_by = 5
+
+    def get_queryset(self):
+        # Получаем обычный запрос
+        queryset = super().get_queryset()
+        # Используем наш класс фильтрации.
+        # self.request.GET содержит объект QueryDict, который мы рассматривали
+        # в этом юните ранее.
+        # Сохраняем нашу фильтрацию в объекте класса,
+        # чтобы потом добавить в контекст и использовать в шаблоне.
+        self.filterset = NewsFilter(self.request.GET, queryset)
+        # Возвращаем из функции отфильтрованный список статей
+        return self.filterset.qs
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['all_news'] = Post.objects.all()
+        context['filterset'] = self.filterset
+        return context
+
+
+
+class PostDetail(DetailView):
+    model = Post
+    # ordering = 'title'
+    template_name = 'post.html'
+    context_object_name = 'post'
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['time_now'] = \
+            datetime.utcnow()
+        context['next_info'] = None
+        return context
+
+class Search(ListView):
+    # Указываем модель, объекты которой мы будем выводить
+    model = Post
+    # Поле, которое будет использоваться для сортировки объектов
+    ordering = '-dateCreation'
+    # Указываем имя шаблона, в котором будут все инструкции о том,
+    # как именно пользователю должны быть показаны наши объекты
+    # queryset = Post.objects.all()
+    template_name = 'news_search.html'
+    # Это имя списка, в котором будут лежать все объекты.
+    # Его надо указать, чтобы обратиться к списку объектов в html-шаблоне.
+    context_object_name = 'news'
+    paginate_by = 5
+
+    def get_queryset(self):
+        queryset = super().get_queryset()
+        self.filterset = NewsFilter(self.request.GET, queryset)
+        return self.filterset.qs
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['time_now'] = \
+            datetime.utcnow()
+        context['next_info'] = None
+        context['filterset'] = self.filterset
+        return context
+
+class NewsCreate(CreateView):
+    form_class = PostForm
+    model = Post
+    template_name = 'news_create.html'
+
+
+class NewsUpdate(UpdateView):
+    form_class = PostForm
+    model = Post
+    template_name = 'news_edit.html'
+
+class NewsDelete(DeleteView):
+    model = Post
+    template_name = 'news_delete.html'
+    success_url = reverse_lazy('news')
+
+
+
